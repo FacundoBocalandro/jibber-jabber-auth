fda8618ad4e0d7752e7e61d7c248ac2e32da9a76
Facundo Bocalandro <facundo@bocalandro.com.ar>
2021-05-09 17:29:27 -0300

    initial commit

---
+++ src/main/java/com/ingsis/jibberjabberauth/JibberJabberAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ingsis.jibberjabberauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberJabberAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibberJabberAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ingsis/jibberjabberauth/JibberJabberAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingsis.jibberjabberauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibberJabberAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

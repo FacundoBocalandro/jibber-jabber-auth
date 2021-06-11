f91df1bc0ed9c89024a68db9a268c79d725bb347
Facundo Bocalandro <facundo@bocalandro.com.ar>
2021-06-11 00:30:38 -0300

    Integration with proxy

--- src/main/java/com/ingsis/jibberjabberauth/security/SecurityConfiguration.java
+++ src/main/java/com/ingsis/jibberjabberauth/security/SecurityConfiguration.java
@@ -42,6 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers(HttpMethod.POST,"/register").permitAll()
                 .antMatchers(HttpMethod.POST,"/login").permitAll()
+                .antMatchers(HttpMethod.GET,"/validate-token").permitAll()
                 .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**","/swagger-resources/configuration/ui","/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement()
